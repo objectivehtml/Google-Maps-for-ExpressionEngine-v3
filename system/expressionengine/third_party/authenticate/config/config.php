b320a5e1f9adfc591763fa05289424f41b2897ab
Justin Kimbrell <justin@inthesaucepan.com>
2012-05-26 17:43:25 -0400

    Delete EE system files

--- system/expressionengine/third_party/authenticate/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-/*-------------------------------------------    
-	Remove the ugly redirect screen
--------------------------------------------*/
-
-$config['remove_redirect_screen'] = TRUE;
