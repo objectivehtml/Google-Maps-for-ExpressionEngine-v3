6dee6d67effcedcbd5489ba90b8401d25aa5631c
Justin Kimbrell <justin@objectivehtml.com>
2012-12-04 23:09:24 -0500

    Increased verson

--- system/gmap/config/gmap_config.php
+++ system/gmap/config/gmap_config.php
@@ -4,8 +4,8 @@
  *	Version and Build
 */
 
-$config['gmap_version']	= '3.1.0';
-$config['gmap_build']	= '20121203';
+$config['gmap_version']	= '3.1.0.1';
+$config['gmap_build']	= '20121204';
 
 /* 
  *	Protect JavaScript
